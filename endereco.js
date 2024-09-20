c88148d1d70e94be69ad855a027e32eb17db6e73
Karen <karen.millena@alunoescolassociais.org.br>
2024-09-20 07:31:10 -0300

    adiciona js limpar formulario e preencher formulario cep valido e numero

--- endereco.js
+++ endereco.js
@@ -16,3 +16,13 @@ const limparFormulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho do CEP
 const cepValido =(cep) => cep.length == 8 && eNumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('CEP').value = endereco.CEP; // Coloca o valor CEP da API dentro do campo CEP formulário
+    document.getElementById('rua').value = endereco.rua;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('complemento').value = endereco.complemento;
+    document.getElementById('cidade').value = endereco.cidade;
+    document.getElementById('estado').value = endereco.estado;
+}
